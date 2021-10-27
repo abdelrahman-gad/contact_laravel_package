3f6c8f3673f4b314be688412e7ff08d1a26568f7
abdelrahman azmy gad <abdelrahmangad95@gmail.com>
2021-10-27 16:27:36 +0200

    iitial commit

---
+++ package/contact/src/ContactServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Gad\Contact;
+use Carbon\Laravel\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider{
+
+    public function boot(){
+ 
+     $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+     $this->loadViewsFrom(__DIR__.'./views','contact');
+     $this->loadMigrationsFrom(__DIR__.'./database/migrations');
+    }
+
+    public function register(){
+        
+    }
+}
